56f10f38ff4839d070ac986a930c0d2ad7e12aa9
Louis Wyborn <louiswyborn@gmail.com>
2021-02-20 17:58:36 +0100

    Further refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,9 @@ mod utils;
 
 use crate::draw::*;
 use crate::raytracing::*;
-use std::time::Instant;
+use pixels::Error;
 
-fn main() {
-    let start_load = Instant::now();
-
+fn main() -> Result<(), Error> {
     let p0 = cgmath::Vector3 {
         x: 0.0,
         y: 0.0,
@@ -19,16 +17,9 @@ fn main() {
     };
     let camera = Camera::new(p0, 1.0, cgmath::Deg(0.0));
 
-    let mut visualiser = Visualiser::new(SCREEN_HEIGHT, SCREEN_WIDTH, camera);
+    let visualiser = Visualiser::new(SCREEN_HEIGHT, SCREEN_WIDTH, camera);
 
     let scene = crate::cornell_box::get_scene();
 
-    let start_render = Instant::now();
-
-    draw::render_scene(visualiser, scene);
-
-    let render_time = start_render.elapsed().as_millis();
-    let total_time = start_load.elapsed().as_millis();
-    println!("Render time: {}ms", render_time);
-    println!("Total time: {}ms", total_time);
+    return draw::render_scene(visualiser, scene);
 }
